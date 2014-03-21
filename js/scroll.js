53c0635f2ecb914982456695b5972e84e6e7be54
Juho Kim <truepanic@gmail.com>
2014-03-21 12:30:48 -0400

    deceleration support

--- js/scroll.js
+++ js/scroll.js
@@ -27,6 +27,10 @@ var KineticScrolling = function ($, window, document) {
 
         // unit: ms
         timeConstant: 325,
+
+        // DOI snapping position: when decelerating or snapping around DOI points,
+        // where should it be relative to the screen size? (in pixels from the top)
+        doiSnappingPosition: 0,
     };
 
     var view, indicator, relative,
@@ -99,6 +103,7 @@ var KineticScrolling = function ($, window, document) {
     // The page layout has changed, so refresh the data to reflect the updated layout.
     function refresh() {
         mapDOMtoPosition();
+        resetDirty();
         markPeaks();
     }
 
@@ -109,18 +114,27 @@ var KineticScrolling = function ($, window, document) {
         var node;
         for (i = 0; i < config.doi.length; i++) {
             node = config.doi[i]["node"];
+            config.doi[i]["id"] = "doi-" + i;
             config.doi[i]["y"] = node.offsetTop;
         }
-        console.log(config.doi);
+        config.doiSnappingPosition = screen.height / 2.5;
+        console.log(config.doi, config.doiSnappingPosition);
+        // TODO sort based on offset?
     }
 
+    function resetDirty() {
+        var i;
+        var node;
+        for (i = 0; i < config.doi.length; i++) {
+            config.doi[i]["dirty"] = false;
+        }
+    }
 
     function ypos(e) {
         // touch event
         if (e.targetTouches && (e.targetTouches.length >= 1)) {
             return e.targetTouches[0].clientY;
         }
-
         // mouse event
         return e.clientY;
     }
@@ -144,26 +158,72 @@ var KineticScrolling = function ($, window, document) {
         velocity = 0.8 * v + 0.2 * velocity;
     }
 
+    var penaltyCount = 0;
+    var recentDelta;
+    var timeFreezeElapsed = 0;
+    var timeFreeze = 0;
+    var isTimeUpdated = true;
     function autoScroll() {
-        var elapsed, delta, snappingIntensity = 0.5;
-
+        penaltyCount -= 1;
+        if (penaltyCount == 0 && !isTimeUpdated) {
+            // time interval where no action took place.
+            timeFreezeElapsed += Date.now() - timeFreeze;
+            console.log("FREEZE", timeFreezeElapsed);
+            isTimeUpdated = true;
+            // requestAnimationFrame(autoScroll);
+            // return;
+        }
+        var elapsed, delta, snappingIntensity = 0.5, peaksInRange;
         if (amplitude) {
-            elapsed = Date.now() - timestamp;
+            // time elapsed from the initial release
+            elapsed = Date.now() - timestamp - timeFreezeElapsed;
             delta = -amplitude * Math.exp(-elapsed / config.timeConstant);
-            console.log(delta);
-            if (peakIncluded) {
+            peaksInRange = getPeaksInRange(offset, target + delta);
+            // if (peakIncluded) {
+            if (peaksInRange["peaks"].length > 0) {
                 snappingIntensity = 10;
             }
             if (delta > snappingIntensity || delta < -snappingIntensity) {
-                scroll(target + delta);
+                // if peak is included in this turn, decelerate based on DOI
+                console.log("cur", parseInt(offset), "tar", target, "del", parseInt(delta), peaksInRange["peaks"].length > 0);
+                if (peaksInRange["peaks"].length > 0) {
+                    for (var i = 0; i < peaksInRange["peaks"].length; i++) {
+                        if (!peaksInRange["peaks"][i]["dirty"]) {
+                            recentDelta = delta;
+                            scroll(target + recentDelta);
+                            // update target based on how much we lost due to peak friction
+                            // target += 0.95 * delta;
+                            penaltyCount = 20;
+                            isTimeUpdated = false;
+                            timeFreeze = Date.now();
+                            $("#" + peaksInRange['peaks'][i]['id']).stop(true, true);
+                            $("#" + peaksInRange['peaks'][i]['id']).show().fadeOut(2000);
+                            peaksInRange["peaks"][i]["dirty"] = true;
+                            console.log("DECEL", peaksInRange["peaks"][i]["id"]);
+                        }
+                    }
+                } else {
+                    if (penaltyCount > 0)
+                        scroll(target + recentDelta);
+                    else
+                        scroll(target + delta);
+                }
                 requestAnimationFrame(autoScroll);
             } else {
+                console.log("TARGET reached");
                 scroll(target);
             }
         }
     }
 
     function tap(e) {
+        // custom flag reset
+        resetDirty();
+        timeFreezeElapsed = 0;
+        timeFreezeElapsed = 0;
+        timeFreeze = 0;
+        isTimeUpdated = true;
+
         pressed = true;
         peakIncluded = false;
         reference = ypos(e);
@@ -194,29 +254,32 @@ var KineticScrolling = function ($, window, document) {
         return false;
     }
 
-    function getPeak(n1, n2) {
+
+    function getPeaksInRange(n1, n2) {
         var smaller = n1 < n2 ? n1 : n2;
         var larger = n1 >= n2 ? n1 : n2;
-        var peak;
+        var peaks = [];
         var i;
+        var point;
         for (i = 0; i < config.doi.length; i++) {
             // console.log(config.doi[i]);
-            if (smaller <= config.doi[i]["y"] && config.doi[i]["y"] <= larger) {
-                peak = config.doi[i]["y"];
+            point = config.doi[i]["y"] - config.doiSnappingPosition;
+            if (smaller <= point && point <= larger) {
+                peaks.push(config.doi[i]);
                 // direction-aware. when swiping up, get the last one.
-                if (n1 < n2) {
-                    break;
-                }
+                // if (n1 < n2) {
+                //     break;
+                // }
             }
         }
-        console.log(smaller, larger, peak);
-        return peak;
+        // console.log(smaller, larger, peaks);
+        return {"peaks": peaks, "isReversed": (n1 > n2)};
     }
 
     function markPeaks() {
         var i;
         for (i = 0; i < config.doi.length; i++) {
-            $("<div/>").addClass("peak").css("top", config.doi[i]["y"]).appendTo("#view");
+            $("<div/>").addClass("peak").attr("id", config.doi[i]["id"]).css("top", config.doi[i]["y"]).appendTo("#view");
         }
     }
 
@@ -230,12 +293,15 @@ var KineticScrolling = function ($, window, document) {
         }
         target = Math.round(offset + amplitude);
         console.log(target);
-        var peakTarget = getPeak(offset, target);
-        if (typeof peakTarget !== "undefined") {
-            peakIncluded = true;
-            target = peakTarget;
-        }
-        console.log("peak", target);
+        // var peakTarget = getPeaksInRange(offset, target);
+        // if (peakTarget["peaks"].length > 0) {
+        //     peakIncluded = true;
+        //     if (peakTarget["isReversed"])
+        //         target = peakTarget[peakTarget["peaks"].length - 1];
+        //     else
+        //         target = peakTarget[0];
+        // }
+        // console.log("peak", target);
         amplitude = target - offset;
         timestamp = Date.now();
         requestAnimationFrame(autoScroll);
